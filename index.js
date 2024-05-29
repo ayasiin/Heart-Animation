78ef5ce01291b419e3852e19b782c233cfa964c0
Abubakar yasin <ayasiin@hotmail.com>
2024-05-29 06:36:42 +0100

    first commit

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
-const bodyEl = document.querySelector('body')
-bodyEl.addEventListener('mousemove',(event)=>{
-    const xPos = event.offsetX
-    const ypost = event.offsetY
-    const spanEl = document.createElement('span')
-    spanEl.style.left = xPos + 'px';
-    spanEl.style.top = ypost + 'px';
-    const size = Math.random()*100
-    spanEl.style.width = size + 'px'
-    spanEl.style.height = size + 'px'
-    bodyEl.appendChild(spanEl)
-    setTimeout(()=>{
-        spanEl.remove();
-    },3000)
-    console.log(spanEl)
-})
+// Counter Program
+const decreaseBtn = document.getElementById('decreaseBtn')
+const increaseBtn = document.getElementById('increaseBtn')
+const countLabel = document.getElementById('countLabel')
+const btnContainer = document.getElementById('btnContainer')
+
+
+let count = 0;
+increaseBtn.onclick = function () {
+    count++;
+    countLabel.textContent = count;
+}
+decreaseBtn.onclick = function(){
+    count--;
+    countLabel.textContent= count
+}
+resetBtn.onclick = function(){
+    count = 0;
+    countLabel.textContent = 0;
+}
+btnContainer.onclick = function (){
+btnContainer.style.backgroundColor = 'gray';
+resetBtn.style.color ='red'
+decreaseBtn.style.color = 'black'
+increaseBtn.style.color = 'green'
+}
